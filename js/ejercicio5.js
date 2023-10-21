16bf146c17c195f30849d5f9dd0dba94cbd194f7
fatiperea <fatimaperea3pppp0@gmail.com>
2023-10-21 11:27:28 -0300

    creo el archivo ejercicio5.js

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+let num1=parseInt(prompt("Ingrese el primer numero: "));
+let num2=parseInt(prompt("Ingrese el segundo numero: "));
+document.write("La suma de los números ingresados es: ",num1 + num2);
