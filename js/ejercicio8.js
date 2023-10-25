6095ce0ef0d287b723b804f74e1e01f27d2e46cf
fatiperea <fatimaperea3pppp0@gmail.com>
2023-10-25 16:05:18 -0300

    creo el archivo ejercicio8.js

---
+++ js/ejercicio8.js
@@ -0,0 +1,9 @@
+const num=parseInt(prompt("Ingrese un numero: "));
+
+if(num%2==0)
+{
+    document.write("El numero "+num+" es divisible por 2");
+}else
+{
+    document.write("El numero "+num+" no es divisible por 2");
+}
