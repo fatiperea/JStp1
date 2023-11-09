695d1e62fa9384d35de14493b6c7c86112695502
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-09 13:38:51 -0300

    creo archivo ejercicio9.js

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,18 +1,22 @@
 let frase = prompt("Ingrese frase: ");
+let vocales = " ";
+let aux = false;
 
-for (let i = 0; i < frase.length; i++) {
-  switch (frase.charAt(i)) {
-    case "a":
-    case "e":
-    case "i":
-    case "o":
-    case "u":
-      document.write(frase.charAt(i));
-      break;
-
-    default:
-      document.write("No ingreso una frase con vocales"+"<br>");
-      break;
+if (isNaN(frase)) {
+  for (let i = 0; i < frase.length; i++) {
+    switch (frase.charAt(i)) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+        vocales = vocales + frase.charAt(i);
+        aux = true;
+        break;
+    }
+    
   }
-  
-}
+  if(aux) document.write("Vocales de la frase: ", vocales);
+  else document.write("No ingreso vocales");
+} else document.write("No ingreso frase");
+
